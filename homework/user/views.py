93ac857738e41748fbea2ee9bea85b0acdc58cb7
Alexander Mangazeev <alex3287@bk.ru>
2022-04-05 19:28:30 +0300

    create views

--- homework/user/views.py
+++ homework/user/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import MyUser
+from .serializers import MyUserModelSerializer
 
-# Create your views here.
+
+class MyUserModelViewSet(ModelViewSet):
+    queryset = MyUser.objects.all()
+    serializer_class = MyUserModelSerializer
