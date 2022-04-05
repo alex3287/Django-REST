0518eaaa1194ed092a409c07b4bf4dc0faff4828
Alexander Mangazeev <alex3287@bk.ru>
2022-04-05 19:34:45 +0300

    add urls

--- homework/homework/urls.py
+++ homework/homework/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from user.views import MyUserModelViewSet
+
+
+router = DefaultRouter()
+router.registry('user', MyUserModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-user/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
 ]
