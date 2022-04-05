944208a12f26378492446a061889db301d48a311
Alexander Mangazeev <alex3287@bk.ru>
2022-04-05 19:35:15 +0300

    small change

--- homework/user/models.py
+++ homework/user/models.py
@@ -5,4 +5,4 @@ class MyUser(models.Model):
     username = models.CharField(max_length=35)
     firstname = models.CharField(max_length=65)
     lastname = models.CharField(max_length=65)
-    email = models.CharField(max_length=65)
+    email = models.UUIDField(max_length=60)
