98bc03e8623bcdfe7bf7d4b1f029871723c587fa
Alexander Mangazeev <alex3287@bk.ru>
2022-04-11 10:30:19 +0300

    render datas from array

--- homework/frontend/src/App.js
+++ homework/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import UserList from "./components/User";
 
 class App extends React.Component {
   constructor(props) {
@@ -9,11 +10,34 @@ class App extends React.Component {
       'users': []
     }
   }
+  componentDidMount() {
+    const users = [
+         {
+        "url": "http://127.0.0.1:8000/api/user/1/",
+        "username": "alex",
+        "firstname": "alex",
+        "lastname": "li",
+        "email": "alex@mail.ru"
+        },
+        {
+        "url": "http://127.0.0.1:8000/api/user/2/",
+        "username": "al",
+        "firstname": "Max",
+        "lastname": "Petrov",
+        "email": "petrov@mail.ru"
+        },
+    ]
+      this.setState(
+          {
+              'users': users
+          }
+      )
+  }
 
   render() {
     return (
         <div>
-          Main App
+          <UserList users={this.state.users} />
         </div>
     )
   }
